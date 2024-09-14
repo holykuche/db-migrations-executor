ffda936107157895703cdfc66516e0238339c67d
Nikita Temnikov <nikita@investbanq.com>
2024-09-14 17:32:28 +0500

    Defined project building

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const DeclarationBundlerPlugin = require("types-webpack-bundler")
+
+module.exports = {
+    mode: "production",
+    target: "node",
+    entry: [
+        path.resolve(__dirname, "index.ts"),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            },
+        ],
+    },
+    resolve: {
+        modules: [
+            path.resolve(__dirname, "src"),
+            path.resolve(__dirname, "node_modules"),
+        ],
+        extensions: [ '.js', ".ts" ]
+    },
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new DeclarationBundlerPlugin({
+            moduleName: "\"db-migrations-executor\"",
+            out: "./index.d.ts"
+        }),
+        new CleanWebpackPlugin(),
+    ],
+};
